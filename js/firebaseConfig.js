6f669c3a124518e17a24d8686b3f99174a1a7b9d
zacshreddar <njorogeian48@gmail.com>
2026-01-06 21:53:49 +0300

    Update and rename firebase.js to firebaseConfig.js

---
+++ js/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Firebase config and init
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore, doc, setDoc, getDoc, onSnapshot, updateDoc, arrayUnion, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "snapmap-lite.firebaseapp.com",
+  projectId: "snapmap-lite",
+  storageBucket: "snapmap-lite.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_ID",
+  appId: "YOUR_APP_ID"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+
+
