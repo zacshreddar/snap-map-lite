667029072c172dd959237077f3f727e34b5b81ea
zacshreddar <njorogeian48@gmail.com>
2026-01-06 15:28:48 +0300

    Update categories.js

--- js/categories.js
+++ js/categories.js
@@ -1,60 +1,16 @@
-// js/categories.js
-export const categories = [
-  // Food & Drinks
-  'Street Food',
-  'Fresh Produce',
-  'Homemade Meals',
-  'Snacks & Soft Drinks',
-  'Coffee / Juice Stands',
+export const categoryGroups = {
+  'Food & Drinks': [
+    'Street Food',
+    'Fresh Produce',
+    'Homemade Meals',
+    'Snacks & Soft Drinks',
+    'Coffee / Juice Stands'
+  ],
+  'Fashion & Accessories': [
+    'Thrift Clothes',
+    'Sneakers / Slides',
+    'Jewelry / Watches'
+  ],
+  // etc...
+};
 
-  // Fashion & Accessories
-  'Thrift Clothes',
-  'Sneakers / Slides',
-  'Jewelry / Watches',
-  'Caps, Shades & Belts',
-  'Bags & Backpacks',
-
-  // Services & Repairs
-  'Barber / Salon Services',
-  'Phone Repair / Charging Stations',
-  'Shoe Repair',
-  'Tailoring & Fitting',
-  'Car Wash / Detailing',
-
-  // Electronics & Gadgets
-  'Used Phones / Accessories',
-  'Bluetooth Speakers / Earphones',
-  'Chargers, Cables, Adapters',
-  'Gamepads, TVs, Radios',
-
-  // Construction & Hardware
-  'Building Materials',
-  'Electricals',
-  'Plumbing Supplies',
-  'Paint & Tools',
-  'Labour Listings',
-
-  // Transport & Delivery
-  'Boda Boda Delivery',
-  'Parcel Pickups',
-  'Moving Services',
-  'Car / Bike Hire',
-
-  // Farming & Agro
-  'Animal Feed',
-  'Seeds & Fertilizers',
-  'Small Farm Equipment',
-  'Fresh Farm Produce',
-
-  // Beauty & Health
-  'Cosmetics & Perfumes',
-  'Herbal Products',
-  'Gym / Fitness Coaching',
-  'Supplements',
-
-  // Entertainment & Hobbies
-  'DJ / Event Services',
-  'Game Consoles / CDs',
-  'Art / Handmade Crafts',
-  'Photography / Video Gigs'
-];
